3692ab8629bc838be1e630c680d1760d596f5e16
kernel <diegocastellanos551@gmail.com>
2020-09-24 21:41:44 -0500

    bleu0

--- supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -11,7 +11,7 @@ def uni_bleu(references, sentence):
     calculates the unigram BLEU score for a sentence
     """
 
-    cLen = len(sentence)
+    candidateLen = len(sentence)
     refLen = []
     clipped = {}
 
@@ -20,14 +20,16 @@ def uni_bleu(references, sentence):
 
         for w in refs:
             if w in sentence:
-                if not clipped.keys() == w:
+                if not clipped.keys() == w: 
                     clipped[w] = 1
+
     clipped_count = sum(clipped.values())
-    closest_refLen = min(refLen, key=lambda m: abs(m - cLen))
-    if cLen > closest_refLen:
+    closest_refLen = min(refLen, key=lambda m: abs(m - candidateLen))
+
+    if candidateLen > closest_refLen:
         bp = 1
     else:
-        bp = np.exp(1 - float(closest_refLen) / float(cLen))
-    bleuScore = bp * np.exp(np.log(clipped_count / cLen))
+        bp = np.exp(1 - float(closest_refLen) / float(candidateLen))
+    bleuScore = bp * np.exp(np.log(clipped_count / candidateLen))
 
     return bleuScore
