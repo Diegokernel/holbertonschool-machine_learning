faa47854f955c6e8f81d1b3774861afdb5e9d528
Diego <777@holbertonschool.com>
2020-09-25 21:59:19 -0500

    nlp

--- supervised_learning/0x11-attention/0-rnn_encoder.py
+++ supervised_learning/0x11-attention/0-rnn_encoder.py
@@ -15,14 +15,14 @@ class RNNEncoder(tf.keras.layers.Layer):
     def __init__(self, vocab, embedding, units, batch):
         """
         class constructor
-        :param vocab: integer represents size of input vocab
+            return full sequence of outputs
+            return hidden state
+            recurrent weights intialized with glorot_uniform
         """
         super(RNNEncoder, self).__init__()
-
         self.batch = batch
         self.units = units
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
-
         self.gru = tf.keras.layers.GRU(units=units,
                                        recurrent_initializer="glorot_uniform",
                                        return_sequences=True,
@@ -32,18 +32,16 @@ class RNNEncoder(tf.keras.layers.Layer):
         """
         Public instance method
         Initiailizes the hidden states for RNN cell to a tensor of zeros
+        :return: a tensor shape(batch, units)
+                contains: initialized hidden states
         """
         initializer = tf.keras.initializers.Zeros()
-
         rnnten = initializer(shape=(self.batch, self.units))
-
         return rnnten
 
     def call(self, x, initial):
         """
         Public instanc method
-        :param x: tensor shape(batch, input_seq_len, units)
         """
         outputs, hidden = self.gru(self.embedding(x), initial_state=initial)
-
         return outputs, hidden
