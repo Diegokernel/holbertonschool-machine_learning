0eeb99cec58e47478a720ba649076c44b73a80c1
Diego <777@holbertonschool.com>
2020-09-25 22:10:36 -0500

    nlp

--- supervised_learning/0x11-attention/0-rnn_encoder.py
+++ supervised_learning/0x11-attention/0-rnn_encoder.py
@@ -1,47 +1,48 @@
-#!/usr/bin/evn python3
-"""
-Class RNNEncoder
-inherits from tensorflow.keras.layers.Layer to encode
-machine translators
-"""
+#!/usr/bin/env python3
+""" RNN encoder"""
 
 import tensorflow as tf
 
 
 class RNNEncoder(tf.keras.layers.Layer):
     """
-    RNNcoder Class
+    class rnn encoder
     """
     def __init__(self, vocab, embedding, units, batch):
         """
-        class constructor
-            return full sequence of outputs
-            return hidden state
-            recurrent weights intialized with glorot_uniform
+        Inititalizer function
+        Args:
+            batch: integer representing the batch size
         """
         super(RNNEncoder, self).__init__()
         self.batch = batch
         self.units = units
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
-        self.gru = tf.keras.layers.GRU(units=units,
-                                       recurrent_initializer="glorot_uniform",
+        self.gru = tf.keras.layers.GRU(units,
+                                       recurrent_initializer='glorot_uniform',
                                        return_sequences=True,
                                        return_state=True)
 
     def initialize_hidden_state(self):
         """
-        Public instance method
-        Initiailizes the hidden states for RNN cell to a tensor of zeros
-        :return: a tensor shape(batch, units)
-                contains: initialized hidden states
+        Inititalize hidden states
+        Returns: tensor of shape (batch, units) containing the initialized
+                 hidden states
         """
         initializer = tf.keras.initializers.Zeros()
-        rnnten = initializer(shape=(self.batch, self.units))
-        return rnnten
+        hiddenQ = initializer(shape=(self.batch, self.units))
+        return hiddenQ
 
     def call(self, x, initial):
         """
-        Public instanc method
+        Call methods
+        Returns: outputs, hidden
+                Outputs: Tensor of shape (batch, input_seq_len, units)
+                         containing the outputs of the encoder
+                Hidden: Tensor of shape (batch, units) containing the last
+                        hidden state of the encoder
         """
-        outputs, hidden = self.gru(self.embedding(x), initial_state=initial)
-        return outputs, hidden
+        embedding = self.embedding(x)
+        outputs, last_hiddenQ = self.gru(embedding,
+                                         initial_state=initial)
+        return outputs, last_hiddenQ
