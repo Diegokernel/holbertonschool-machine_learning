c83a44d5fff9bfe5133b3ee5bfff8c395292c9c9
Diego <777@holbertonschool.com>
2020-09-25 22:16:02 -0500

    nlp

--- supervised_learning/0x11-attention/4-positional_encoding.py
+++ supervised_learning/0x11-attention/4-positional_encoding.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 """
-function calculates the positional encoding got a transformer
+Creates the function positional encoding
 """
 import numpy as np
-import tensorflow as tf
 
 
 def positional_encoding(max_seq_len, dm):
     """
-    calculates positional encoding for a transformer
+    Calculates the positional encoding for a transformer
     """
-    posEncoding = np.zeros([max_seq_len, dm])
+    poem = np.zeros((max_seq_len, dm))
 
-    for i in range(dm):
-        for pos in range(max_seq_len):
-            posEncoding[pos, i] = pos / np.power(10000, (2 * (i // 2)) / (dm))
+    for position in range(max_seq_len):
+        for i in range(0, dm, 2):
+            div = np.exp(i * -np.log(10000.0) / dm)
+            poem[position, i] = (
+                np.sin(position * div))
+            poem[position, i + 1] = (
+                np.cos(position * div))
 
-    posEncoding[:, 0::2] = np.sin(posEncoding[:, 0::2])
-    posEncoding[:, 1::2] = np.cos(posEncoding[:, 1::2])
-
-    return posEncoding
+    return poem
