becc48be1a919db9483160d0602df809d501c783
Diego <777@holbertonschool.com>
2020-09-25 22:25:44 -0500

    att

--- supervised_learning/0x11-attention/10-transformer_decoder.py
+++ supervised_learning/0x11-attention/10-transformer_decoder.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """
-Class Decoder
+Creates encoder for transformer
 """
-
 import tensorflow as tf
 positional_encoding = __import__('4-positional_encoding').positional_encoding
 DecoderBlock = __import__('8-transformer_decoder_block').DecoderBlock
@@ -10,40 +9,39 @@ DecoderBlock = __import__('8-transformer_decoder_block').DecoderBlock
 
 class Decoder(tf.keras.layers.Layer):
     """
-    class c reates the decoder for a tranformer
+    Class to create an encoder for a transformer
     """
-    def __init__(self, N, dm, h, hidden, target_vocab,
-                 max_seq_len, drop_rate=0.1):
+
+    def __init__(self, N, dm, h, hidden, target_vocab, max_seq_len,
+                 drop_rate=0.1):
         """
-        class constructor
-        param:: N: number of blocks in the encoder
+        Class constructor
+        :param dm: an integer representing
         """
         super(Decoder, self).__init__()
-
         self.N = N
         self.dm = dm
         self.embedding = tf.keras.layers.Embedding(target_vocab, dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
-        self.blocks = [DecoderBlock(dm, h, hidden, drop_rate)
-                       for m in range(N)]
 
+        self.blocks = [DecoderBlock(dm, h, hidden,
+                                    drop_rate) for _ in range(N)]
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
-        """the second multi
-                        head attention layer
-        Returns: tensor of shape (batch, target_seq_len, dm)
-                 containing the decoder output
         """
-        seq_len = tf.shape(x)[1]
-
+        Function to create the decoder block for the transformer
+        :param x: a tensor of shape (batch, target_seq_len, dm)containing the
+        input to the decod
+        """
+        seq_len = x.shape[1]
         x = self.embedding(x)
-        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
-        x += self.positional_encoding[:seq_len]
+        x = x * tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        x = x + self.positional_encoding[:seq_len]
         x = self.dropout(x, training=training)
 
-        for idx in range(self.N):
-            dec_output = self.blocks[idx](x, encoder_output, training,
-                                          look_ahead_mask, padding_mask)
+        for i in range(self.N):
+            x = self.blocks[i](x, encoder_output,
+                               training,look_ahead_mask, padding_mask)
 
-        return dec_output
+        return x
--- supervised_learning/0x11-attention/9-transformer_encoder.py
+++ supervised_learning/0x11-attention/9-transformer_encoder.py
@@ -1,43 +1,49 @@
 #!/usr/bin/env python3
 """
-Class Encoder
+This module has the class DecoderBlock
 """
 
 import tensorflow as tf
+
 positional_encoding = __import__('4-positional_encoding').positional_encoding
 EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 
 class Encoder(tf.keras.layers.Layer):
+    """
+    This class perfoms encoder
+    """
+
     def __init__(self, N, dm, h, hidden, input_vocab,
                  max_seq_len, drop_rate=0.1):
         """
-        Class Constructor
+        All begins here
         """
         super(Encoder, self).__init__()
 
-        self.N = N
         self.dm = dm
-        self.embedding = tf.keras.layers.Embedding(input_vocab, self.dm)
-        self.positional_encoding = positional_encoding(max_seq_len, self.dm)
+        self.N = N
 
+        self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
+        self.positional_encoding = positional_encoding(max_seq_len, self.dm)
         self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
-                       for m in range(self.N)]
+                       for _ in range(N)]
 
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask):
         """
-        Public Instance Method
+        This method call the model
         """
-        seq_len = tf.shape(x)[1]
+        seq_len = x.shape[1]
+
         x = self.embedding(x)
         x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
         x += self.positional_encoding[:seq_len]
 
-        enc_output = self.dropout(x, training=training)
+        x = self.dropout(x, training=training)
 
-        for idx in range(self.N):
-            enc_output = self.blocks[idx](enc_output, training, mask)
+        for i in range(self.N):
+            x = self.blocks[i](x, training, mask)
 
-        return enc_output
+        return x
