43871566d86fd19e212cdedaf6887802598e4d9c
Diego <777@holbertonschool.com>
2020-10-15 15:35:13 -0500

    att

---
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Put a gensim word2vec model in a keras embedding layer"""
+
+
+from tensorflow import keras
+
+
+def gensim_to_keras(model):
+    """Put a gensim word2vec model
+    model::model"""
+
+    return model.wv.get_keras_embedding(train_embeddings=True)
